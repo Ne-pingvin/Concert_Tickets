3b2aa50852f2a680581e13cd0ec1d390ab239450
Ne-pingvin <anathema.an@gmail.com>
2024-10-25 12:20:14 +0200

    the end

--- src/TicketServise.java
+++ src/TicketServise.java
@@ -2,31 +2,43 @@ import java.time.Instant;
 
 public class TicketServise {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Ticket empty = new Ticket();
+        empty.displayInfo();
+
+        Ticket full = new Ticket("1234", "Hall1", 123, 1729851247L, true, 'A', 33.3, 100.0);
+        full.displayInfo();
+
+        Ticket limited = new Ticket("Hall2", 124, 1729851247L);
+        limited.displayInfo();
+
     }
 }
+
 class Ticket{
- private String id;
- private String consertHall;
- private int eventCode;
- private long time = Instant.now().getEpochSecond();
- private boolean isPromo;
- private char stadiumSector;
- private double maxWeight;
- private Instant creationTime;
- private double price;
+     private String id;
+     private String consertHall;
+     private int eventCode;
+     private long time;
+     private boolean isPromo;
+     private char stadiumSector;
+     private double maxWeight;
+     private Instant creationTime;
+     private double price;
+     void displayInfo (){
+         System.out.println("ID: " + id + ", Hall: " + consertHall + ", Event code: " + eventCode + ", Time: " + time + ", Promo: " + isPromo +", Stadium sector: " + stadiumSector +", Max weight: " + maxWeight + ", Creation time: " + creationTime + ", Price: " + price);
+     }
 
  public Ticket(String id, String concertHall, int eventCode, long time, boolean isPromo, char stadiumSector, double maxWeight, double price)
  {
-this.id = id;
-this.consertHall = concertHall;
-this.eventCode = eventCode;
-this.time = time;
-this.isPromo = isPromo;
-this.stadiumSector = stadiumSector;
-this.maxWeight = maxWeight;
-this.price = price;
-this.creationTime = Instant.now();
+    this.id = id;
+    this.consertHall = concertHall;
+    this.eventCode = eventCode;
+    this.time = time;
+    this.isPromo = isPromo;
+    this.stadiumSector = stadiumSector;
+    this.maxWeight = maxWeight;
+    this.price = price;
+    this.creationTime = Instant.now();
  }
     public Ticket(String concertHall, int eventCode, long time)
     {
@@ -35,4 +47,5 @@ this.creationTime = Instant.now();
      this.time = time;
      this.creationTime = Instant.now();
     }
+    public Ticket(){}
  }
